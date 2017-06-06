4c82ecc62b57ce4830c1548b737fe7271fea0177
Janz <mail@ja.nz>
2017-06-06 14:44:46 +0200

    Reducers and Hoc

--- src/App.js
+++ src/App.js
@@ -2,43 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-// Components //
-
+// Components
 const Heading = str => ( 
         <h1>Now Viewing {str}</h1>
-);
-
-const Link = user => (
+),
+Link = user => (
         <a href={`/users/${user.id}`}>{user.name}</a>
 );
 
 // ComponentFactory :: Component a => a -> b
 const ComponentFactory = comp =>
     ({
-        fold: comp, // launch function chain
+        fold: comp, // bound component
         concat: other => // bifold two Components
             ComponentFactory(x => <div>{comp(x)} {other.fold(x)}</div>),
         contramap: statef => // passing state to component
             ComponentFactory(x => comp(statef(x)))
     });
-
-// Reducer :: (a,b) -> a
-const Reducer = g =>
-    ({
-        fold: g,
-        contramap: f =>
-            Reducer((acc, x) => g(acc, f(x))),
-        map: f =>
-            Reducer((acc, x) => f(g(acc, x))),
-        concat: o =>
-            Reducer((acc, x) => o.fold(g(acc, x), x))
-    });
-
-const r = Reducer((acc, x) => acc.concat(x))
-    .contramap(x => `The number is ${x}`)
-    .map(x => x + '! ')
-
-console.log([1,2,3].reduce(r.fold, ''));
 
 
 const state = {
@@ -46,22 +26,118 @@ const state = {
     currentUser: { id: 2, name: 'ja0nz' }
 };
 
-const HeadingPageName = ComponentFactory(Heading).contramap(s => s.pageName),
-      LinkCurrentUser = ComponentFactory(Link).contramap(s => s.currentUser);
+const HeadingBound = ComponentFactory(Heading).contramap(s => s.pageName),
+      LinkBound = ComponentFactory(Link).contramap(s => s.currentUser);
 
 // Merge two components
-const CurrentView = HeadingPageName.concat(LinkCurrentUser);
-
+const CurrentView = HeadingBound.concat(LinkBound);
 
 // Fold view with state
-const App = CurrentView.fold(state);
+const HeadingLinkDiv = CurrentView.fold(state);
 
 export default class extends Component {
     render() {
         return (
             <div className="App">
-                {App} 
+                {HeadingLinkDiv} 
             </div>
         );
     }
 }
+
+/* ----------------------------------------------- */
+// Playing wit Reducers 
+
+// Reducer :: (a,b) -> a
+const Reducer = g =>
+    ({
+        fold: g, // bound reducer
+        contramap: f => // run f on each element before reduce
+            Reducer((acc, x) => g(acc, f(x))),
+        map: f => // reduce first and than map afterwards
+            Reducer((acc, x) => f(g(acc, x))),
+        concat: o => // concat reducers
+            Reducer((acc, x) => o.fold(g(acc, x), x))
+    });
+
+const r = Reducer((acc, x) => acc.concat(x)) // Concat reducer, concat = fold
+    .contramap(x => `The number is ${x}`) // apply on each element
+    .map(x => x + '! ') // reduce + map
+
+console.log([1,2,3].reduce(r.fold, ''));
+
+const appReducer = Reducer((state, action) => {
+    switch (action.type) {
+        case 'set_visibility_filter':
+            return Object.assign({}, state, {
+                visibilityFilter: action.filter
+            })
+        default:
+            return state
+    }
+}),
+todoReducer = Reducer((state, action) => {
+    switch (action.type) {
+        case 'new_todo':
+            const t = { id: 0, title: action.payload.title }
+            return Object.assign({}, state, {
+                todos: state.todos.concat(t)
+            })
+        default:
+            return state
+    }
+});
+
+const todoApp =
+    appReducer
+        .concat(todoReducer)
+        .contramap(action => Object.assign({filter: 'all'}, action))
+        .map(s => Object.assign({}, s, { lastUpdated: Date() }))
+
+const stateTodo = {visibilityFilter: 'complete',
+                   type: 'set_visibility_filter'},
+statefulApp = todoApp.fold(stateTodo);
+
+console.log(statefulApp);
+
+/* ------------------------------ */
+// Higher order Components. Concat with Components
+
+// Hoc :: Component -> Component
+
+const Hoc = g =>
+    ({
+        fold: g,
+        concat: other =>
+            Hoc(x => g(other.fold(x)))
+    });
+
+/*
+ *const hoc = Hoc(withReducer('state', 'dispatch', todoApp.fold, {todos: []}));
+ * 
+ * const Todos = hoc.fold(({ state, dispatch }) => (
+ *     <div>
+ *         <span>Filter: {state.setVisibilityFilter}</span>
+ *         <ul>
+ *             { state.todos.map(t => <li>{t.title}</li>) }
+ *         </ul>
+ *         <button onClick={() =>
+ *             dispatch({type: 'new_todo', payload: {title: 'New todo'}})}>
+ *             Add Todo
+ *         </button>
+ *         <button onClick={() =>
+ *             dispatch({ type: 'set_visibility_filter' })}>
+ *             Set Visibility
+ *         </button>
+ *     </div>
+ * ));
+ * 
+ * 
+ * const TodoComp = Component(classToFn(Todos))
+ * 
+ * App = do
+ * Contramaps with Updaters
+ * concat Components
+ * fold Component with state
+ */
+
